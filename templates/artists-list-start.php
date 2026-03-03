6e59d7f58e09cb76ea5d41ab015a8be52b726cc9
tom wible <air_drummer@verizon.net>
2026-03-02 21:06:31 -0500

    Update artists-list-start.php

--- templates/artists-list-start.php
+++ templates/artists-list-start.php
@@ -1,14 +1,12 @@
 <?php
 	 if(!empty($srchstrgs) || !empty($selected_genres))
 	 {
-		echo "<h4>" . (count($programs)  
+		echo "<h4>" 
+		    . (count($programs)  
 						? count($programs) . " program" . (count($programs) >1 ? "s" : '')
 						: "no programs")
-		    ." matching '";
-		echo implode(" $logic ",
-					 [ implode("' $logic '", $srchstrgs),
-					   implode("' $logic '", $selected_genres)
-					 ]);
-		echo "'</h4>";
-	 }		
+		    . " matching '" . implode("' $logic '",
+					                array_merge($srchstrgs, $selected_genres))
+            . "'</h4>";
+	 }
 ?>
